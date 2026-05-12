2ffc3955e0d4fea68f82ee0cb5884e49416e3989
AcelInv <mdadityaaaa@gmail.com>
2026-05-12 23:09:37 +0700

    Menambahkan route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 3 route mengembalikan string biasa
 Route::get('/hello', function () {
     return 'Hello, World!';
+});
+
+Route::get('/about', function () {
+    return 'Ini adalah halaman About';
+});
+
+Route::get('/contact', function () {
+    return 'Ini adalah halaman Contact';
+});
+
+Route::get('/user/{nama}', function ($nama) {
+    return 'Halo, ' . $nama;
+});
+
+Route::get('/mahasiswa/{nim}', function ($nim) {
+    return 'NIM Mahasiswa: ' . $nim;
 });
